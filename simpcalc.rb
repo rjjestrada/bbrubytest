0e586bdf53c6930e08be33d56a3372aebe6727db
rjjestrada <rjjestrada@gmail.com>
2014-02-10 20:16:26 -0800

    Step 3

--- simpcalc.rb
+++ simpcalc.rb
@@ -1,14 +1,9 @@
 def Add(numbers)
     if numbers.empty?
         puts 0
-    elsif numbers.include? ','
-        numbers = numbers.split(',')
-        numbers.map! { |x| x.to_i }
-        sum = 0
-        numbers.each do |i|
-          sum += i
-        end
-        puts sum
+    elsif numbers.include? ',' or  numbers.include? "\n"
+        numbers = numbers.gsub("\n", ",").split(',').map! { |x| x.to_i }
+        puts numbers.inject(:+)
     else
         puts numbers.to_i
     end
