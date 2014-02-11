89ba77044577825c81f18a4f37cc68cbe6277ece
rjjestrada <rjjestrada@gmail.com>
2014-02-10 20:17:19 -0800

    Step 4

--- simpcalc.rb
+++ simpcalc.rb
@@ -1,6 +1,11 @@
 def Add(numbers)
     if numbers.empty?
         puts 0
+    elsif numbers[0,2] == "//"
+        delimiterend = numbers.index("\n")
+        delimiter = numbers[2..delimiterend-1]
+        numbers = numbers.split("#{delimiter}").map! { |x| x.to_i }
+        puts numbers.inject(:+)
     elsif numbers.include? ',' or  numbers.include? "\n"
         numbers = numbers.gsub("\n", ",").split(',').map! { |x| x.to_i }
         puts numbers.inject(:+)
