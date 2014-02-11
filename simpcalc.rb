0386b32f6470a831ec5ecbaf3a447f3e18a986c5
rjjestrada <rjjestrada@gmail.com>
2014-02-10 20:23:31 -0800

    Step 5

--- simpcalc.rb
+++ simpcalc.rb
@@ -4,8 +4,27 @@ def Add(numbers)
     elsif numbers[0,2] == "//"
         delimiterend = numbers.index("\n")
         delimiter = numbers[2..delimiterend-1]
-        numbers = numbers.split("#{delimiter}").map! { |x| x.to_i }
-        puts numbers.inject(:+)
+        numbers = numbers.split("#{delimiter}")
+        negatives = []
+        numbers.each do |negative|
+            if negative.include? "-"
+                negatives << negative.to_i
+            end
+        end
+        if negatives.empty?
+            numbers.map! { |x| x.to_i }
+            puts numbers.inject(:+)
+        else
+            puts "negatives not allowed: #{negatives}"
+        end
+    elsif numbers.include? "-"
+        negatives = []
+        numbers.gsub("\n", ",").split(',').each do |negative|
+            if negative.include? "-"
+                negatives << negative.to_i
+            end
+        end
+        puts "negatives not allowed: #{negatives}"
     elsif numbers.include? ',' or  numbers.include? "\n"
         numbers = numbers.gsub("\n", ",").split(',').map! { |x| x.to_i }
         puts numbers.inject(:+)
